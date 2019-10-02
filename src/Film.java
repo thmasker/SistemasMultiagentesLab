6f5e129bb46c541bc8e4502d5db65d30250fc202
thmasker <diego.pedregal@alu.uclm.es>
2019-10-02 21:03:40 +0200

    Film hecho, GatherBot hecho

---
+++ src/Film.java
@@ -0,0 +1,25 @@
+public class Film {
+	private String title;
+	private double rating;
+	
+	public Film(String title, double rating) {
+		this.title = title;
+		this.rating = rating;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+}
