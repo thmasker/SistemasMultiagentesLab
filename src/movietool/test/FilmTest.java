ce790714296f1d15b3c91b1cefa92386fed2d420
thmasker <diego.p.hidalgo@gmail.com>
2020-01-12 18:26:49 +0100

    [src] Film overriden methods work as expected

--- src/movietool/test/FilmTest.java
+++ src/movietool/test/FilmTest.java
@@ -2,6 +2,8 @@ package movietool.test;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -16,18 +18,37 @@ public class FilmTest {
 	private ArrayList<Film> films;
 
 	@Before
-	public void setUp() throws IOException {
-        ImdbScrapper bot = new ImdbScrapper();
-        bot.fetch(FilmScrapper.FilmGenre.ACTION, 30);
-        films = bot.selectFilms(20);
+	@SuppressWarnings("serial")
+	public void setUp(){
+		films = new ArrayList<Film>(){
+			{
+				add(new Film("Checking", 9.9));
+			}
+		};
 	}
 
 	@Test
-	public void sortTest(){
-		Collections.sort(films);
+	public void sortTest() throws IOException {
+		ImdbScrapper bot = new ImdbScrapper();
+        bot.fetch(FilmScrapper.FilmGenre.ACTION, 30);
+		ArrayList<Film> movies = bot.selectFilms(20);
+		
+		Collections.sort(movies);
 
-		for(int i = 0; i < films.size() - 1; i++){
-			assertTrue(films.get(i).getRating() >= films.get(i + 1).getRating());
+		for(int i = 0; i < movies.size() - 1; i++){
+			assertTrue(movies.get(i).getRating() >= movies.get(i + 1).getRating());
 		}
+	}
+
+	@Test
+	public void equalsTrue(){
+		Film f = new Film("Checking", 5);
+		assertTrue(films.contains(f));
+	}
+
+	@Test
+	public void equalsFalse(){
+		Film f = new Film("Not this time", 9.9);
+		assertFalse(films.contains(f));
 	}
 }
