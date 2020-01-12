8cb2cf9dd9c9877103c545b8df2966cefd71f181
thmasker <diego.p.hidalgo@gmail.com>
2020-01-12 15:55:36 +0100

    [src] Film. compareTo() and equals() added. Used to order lists of films, and check if films exists on lists

--- src/movietool/utils/Film.java
+++ src/movietool/utils/Film.java
@@ -3,7 +3,7 @@ package movietool.utils;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
-public class Film implements Serializable {
+public class Film implements Comparable<Film>, Serializable {
 	private String title;
 	private double rating;
 	
@@ -26,5 +26,22 @@ public class Film implements Serializable {
 
 	public void setRating(double rating) {
 		this.rating = rating;
+	}
+
+	@Override
+	public int compareTo(Film film) {
+		return (int) ((film.getRating() - this.rating) * 100);
+	}
+
+	@Override
+	public boolean equals(Object obj){
+		if(this == obj){
+			return true;
+		} else if(obj == null || this.getClass() != obj.getClass()){
+			return false;
+		} else {
+			Film film = (Film) obj;
+			return this.title.equals(film.title);
+		}
 	}
 }
